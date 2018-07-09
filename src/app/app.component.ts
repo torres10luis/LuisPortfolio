94710965bc7cd0db474b5af33ad44f9629086738
torres10luis <Luis.angel_15@yahoo.com>
2018-07-09 11:24:37 -0700

    new portfolio up-to-date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { GalleryComponent } from './gallery/gallery.component';
 import { Component } from '@angular/core';
+
+import {trigger, state, animate, transition, keyframes, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GalleryComponent],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+    })),
+    state('large', style({
+        transform: 'scale(1.2)',
+    })),
+    transition('small => large', animate('100ms ease-in')),
+    ]),
+
+  ]
+
 })
 export class AppComponent {
   title = 'app';
+
+  // tslint:disable-next-line:no-inferrable-types
+  state: string = 'small';
+  event: Event;
+
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
